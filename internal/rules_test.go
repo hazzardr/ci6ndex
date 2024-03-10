1c4b0a61f9899d01aec1ab9743e7ec0e8f3acf78
brian <rbrianhazzard@gmail.com>
2024-03-10 19:40:05 -0400

    start tests

--- internal/rules_test.go
+++ internal/rules_test.go
@@ -3,25 +3,24 @@ package internal
 import (
 	"ci6ndex/internal/testhelper"
 	"context"
-	"github.com/magiconair/properties/assert"
 	"github.com/stretchr/testify/suite"
 	"google.golang.org/appengine/log"
 	"testing"
 )
 
-type UsersSuite struct {
+type RulesSuite struct {
 	suite.Suite
 	db            *DatabaseOperations
 	testContainer *testhelper.TestDatabase
 	ctx           context.Context
 }
 
-func TestUsersSuite(t *testing.T) {
-	suite.Run(t, new(UsersSuite))
+func TestRulesSuite(t *testing.T) {
+	suite.Run(t, new(RulesSuite))
 }
 
 // Create a database container for testing, and initialize our client against it.
-func (suite *UsersSuite) SetupSuite() {
+func (suite *RulesSuite) SetupSuite() {
 	suite.ctx = context.Background()
 	testContainer, err := testhelper.CreateTestDatabase(suite.ctx)
 	if err != nil {
@@ -35,7 +34,7 @@ func (suite *UsersSuite) SetupSuite() {
 	}
 }
 
-func (suite *UsersSuite) TearDownSuite() {
+func (suite *RulesSuite) TearDownSuite() {
 	err := suite.testContainer.Terminate(suite.ctx)
 	if err != nil {
 		log.Errorf(suite.ctx, "Error close database container: %v", err)
@@ -44,7 +43,7 @@ func (suite *UsersSuite) TearDownSuite() {
 	suite.db.Close()
 }
 
-func (suite *UsersSuite) TearDownTest() {
+func (suite *RulesSuite) TearDownTest() {
 	err := suite.db.Queries.WipeTables(suite.ctx)
 	if err != nil {
 		log.Errorf(suite.ctx, "Error truncating tables, could not clean DB: %v", err)
@@ -52,40 +51,7 @@ func (suite *UsersSuite) TearDownTest() {
 	}
 }
 
-func (suite *UsersSuite) TestAddUsersFromFile_Success() {
-	t := suite.T()
-	exists := fileExists("testhelper/testdata/single_user.json")
-	suite.True(exists)
-	err := AddUsersFromFile("testhelper/testdata/single_user.json", suite.db)
-	suite.NoError(err)
-	actual, err := suite.db.Queries.GetUserByName(suite.ctx, "username")
-	suite.NoError(err)
-	assert.Equal(t, "username", actual.Name)
-	assert.Equal(t, "discord_name", actual.DiscordName)
-}
-
-func (suite *UsersSuite) TestAddUsersFromFile_MultipleUsers() {
-	t := suite.T()
-	exists := fileExists("testhelper/testdata/multiple_users.json")
-	suite.True(exists)
-	err := AddUsersFromFile("testhelper/testdata/multiple_users.json", suite.db)
-	suite.NoError(err)
-	users, err := suite.db.Queries.GetUsers(suite.ctx)
-	suite.NoError(err)
-	assert.Equal(t, 2, len(users))
-}
-
-func (suite *UsersSuite) TestAddUsersFromFile_UserAlreadyExists() {
-	exists := fileExists("testhelper/testdata/single_user.json")
-	suite.True(exists)
-	err := AddUsersFromFile("testhelper/testdata/single_user.json", suite.db)
-	suite.NoError(err)
-
-	err = AddUsersFromFile("testhelper/testdata/single_user.json", suite.db)
-	suite.Error(err)
-}
-
-func (suite *UsersSuite) TestAddUsersFromFile_FailureCases() {
+func (suite *RulesSuite) TestAddUsersFromFile_FailureCases() {
 	type test struct {
 		name string
 		path string
