8f341c123fef7a57a183122876ca714210f00b16
brian <rbrianhazzard@gmail.com>
2024-10-17 21:48:08 -0400

    wip

--- bot/domain/roll.go
+++ bot/domain/roll.go
@@ -1,11 +1,31 @@
 package domain
 
 import (
+	"ci6ndex-bot/domain/generated"
 	"context"
 	"github.com/pkg/errors"
+	"math/rand/v2"
 )
 
-func (dbo *DatabaseOperations) RollForPlayers(guildId uint64, poolSize uint8) error {
+// OfferPool is a struct that holds a pool of leaders that are valid for a given player
+// The leaders are mapped from a rule to a list of leaders that satisfy the rule
+type OfferPool struct {
+	leaders []generated.Leader
+	rule    Rule
+	player  generated.Player
+}
+
+// NewPool creates a pool of leaders to be offered to a player after filtering based on the rule
+func NewPool(player generated.Player, rule Rule, leaders []generated.Leader) *OfferPool {
+	filteredLeaders := rule.evaluate(player, leaders)
+	return &OfferPool{
+		leaders: filteredLeaders,
+		rule:    rule,
+		player:  player,
+	}
+}
+
+func (dbo *DatabaseOperations) RollForPlayers(guildId uint64, poolSize int) error {
 	ctx := context.TODO()
 	db, err := dbo.getDB(guildId)
 	if err != nil {
@@ -19,4 +39,48 @@ func (dbo *DatabaseOperations) RollForPlayers(guildId uint64, poolSize uint8) er
 	if err != nil {
 		return errors.Wrap(err, "failed to get eligible leaders")
 	}
+
+	// Give everyone a tier 3 or above leader
+	pools := make([]OfferPool, poolSize)
+	for _, player := range players {
+		pool := NewPool(player, &MinTierRule{minTier: 3.0}, leaders)
+		pools = append(pools, *pool)
+	}
+	for i := 1; i < poolSize; i++ {
+		for _, player := range players {
+			// Get the pool for the player
+			pool := NewPool(player, &NoOpRule{}, leaders)
+			pools = append(pools, *pool)
+		}
+	}
+
+	offers := make(map[int64][]generated.Leader)
+	alreadyPicked := make([]generated.Leader, 0)
+	for _, p := range pools {
+		i := rand.IntN(len(p.leaders)) - 1
+		randPick := p.leaders[i]
+		if offers[p.player.ID] == nil {
+			offers[p.player.ID] = make([]generated.Leader, poolSize)
+		}
+		offers[p.player.ID] = append(offers[p.player.ID], randPick)
+		alreadyPicked = append(alreadyPicked, randPick)
+
+	}
+}
+
+func tryPickLeader(p OfferPool, alreadyPicked []generated.Leader) (generated.Leader, error) {
+	i := rand.IntN(len(p.leaders)) - 1
+	randPick := p.leaders[i]
+	if !containsLeader(randPick, alreadyPicked) {
+		return randPick, nil
+	}
+}
+
+func containsLeader(pick generated.Leader, alreadyPicked []generated.Leader) bool {
+	for _, p := range alreadyPicked {
+		if p.ID == pick.ID {
+			return true
+		}
+	}
+	return false
 }
--- bot/domain/rule.go
+++ bot/domain/rule.go
@@ -1,9 +1,21 @@
 package domain
 
-import "ci6ndex-bot/domain/generated"
+import (
+	"ci6ndex-bot/domain/generated"
+	"fmt"
+	"slices"
+)
+
+type FullPoolError struct{}
+
+func (e FullPoolError) Error() string {
+	return fmt.Sprint("attempted to add rule to full pool")
+}
 
 type Rule interface {
 	isValid(player generated.Player, leader generated.Leader) bool
+	// Evaluate should
+	evaluate(player generated.Player, leaders []generated.Leader) []generated.Leader
 }
 
 type MinTierRule struct {
@@ -17,53 +29,17 @@ func (r *MinTierRule) isValid(player generated.Player, leader generated.Leader) 
 	return false
 }
 
+func (r *MinTierRule) evaluate(player generated.Player, leaders []generated.Leader) []generated.Leader {
+	return slices.DeleteFunc(leaders, func(l generated.Leader) bool {
+		return !r.isValid(player, l)
+	})
+}
+
 type NoOpRule struct{}
 
 func (r *NoOpRule) isValid(player generated.Player, leader generated.Leader) bool {
 	return true
 }
-
-type OfferedPicks map[int64][]generated.Leader
-type EligibleLeadersPerPlayer map[int64]map[Rule][]generated.Leader
-
-type RuleSet struct {
-	rules EligibleLeadersPerPlayer
-}
-
-func NewRulesetWithAtLeastOneTierThree(players []generated.Player, poolSize uint8) RuleSet {
-	r := make(map[int64]map[Rule][]generated.Leader)
-	for _, player := range players {
-		r[player.ID] = make(map[Rule][]generated.Leader)
-		for i := 0; i < int(poolSize); i++ {
-			if i == 0 {
-				r[player.ID][&MinTierRule{minTier: 4}] = make([]generated.Leader, 0)
-			}
-			r[player.ID][&NoOpRule{}] = make([]generated.Leader, 0)
-		}
-	}
-	return RuleSet{
-		rules: r,
-	}
-}
-
-func (rs *RuleSet) AddLeaders(leaders []generated.Leader) {
-	for _, leader := range leaders {
-		for _, ruleMap := range rs.rules {
-			for rule := range ruleMap {
-				if rule.isValid(generated.Player{}, leader) {
-					ruleMap[rule] = append(ruleMap[rule], leader)
-				}
-			}
-		}
-	}
-}
-
-func (rs *RuleSet) Evaluate() (OfferedPicks, error) {
-	result := make(map[int64][]generated.Leader)
-	for playerId, ruleMap := range rs.rules {
-		for rule, leaders := range ruleMap {
-
-		}
-	}
-	return result, nil
+func (r *NoOpRule) evaluate(player generated.Player, leaders []generated.Leader) []generated.Leader {
+	return leaders
 }
